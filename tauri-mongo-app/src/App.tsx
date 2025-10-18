18f10e6d38e4ba089c25930b0dbc29ebeb2847c6
AshirbadGudu <ashirbadapanigrahi@gmail.com>
2025-10-19 01:33:44 +0530

    Implement MongoDB connection functionality in the App component and add corresponding Rust command. Update UI to display connection result.

--- tauri-mongo-app/src/App.tsx
+++ tauri-mongo-app/src/App.tsx
@@ -1,14 +1,20 @@
+import { invoke } from "@tauri-apps/api/core";
+import { useState } from "react";
+
 export default function App() {
+  const [result, setResult] = useState<string | null>(null);
   return (
-    <>
+    <main className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
       <button
-        onClick={() => {
-          console.log("Connecting to MongoDB");
+        onClick={async () => {
+          const result = await invoke("connect_mongodb");
+          setResult(result as string);
         }}
         className="bg-blue-500 text-white p-2 rounded-md active:bg-blue-600"
       >
         Connect to MongoDB
       </button>
-    </>
+      {result && <p>{result}</p>}
+    </main>
   );
 }
