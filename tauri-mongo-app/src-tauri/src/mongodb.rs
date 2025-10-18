409c7de2f2a5a4172a3963c3d4b440fd1b7652f7
AshirbadGudu <ashirbadapanigrahi@gmail.com>
2025-10-19 01:57:27 +0530

    Refactor MongoDB connection to use environment variables for configuration, enhancing flexibility and error handling. Default to localhost if MONGODB_URI is not set.

--- tauri-mongo-app/src-tauri/src/mongodb.rs
+++ tauri-mongo-app/src-tauri/src/mongodb.rs
@@ -1,8 +1,17 @@
 use mongodb::Client;
+// Use environment variables
+use dotenv::dotenv;
+use std::env;
 
 #[tauri::command]
 pub async fn connect_mongodb() -> Result<String, String> {
-    let uri = "mongodb://localhost:27017";
+    dotenv().ok();
+    
+    let uri = env::var("MONGODB_URI");
+    if uri.is_err() {
+        return Err(format!("MONGODB_URI is not set"));
+    }
+    let uri = uri.unwrap();
     
     match Client::with_uri_str(uri).await {
         Ok(_client) => Ok("MongoDB connection successful!".to_string()),
@@ -12,7 +21,8 @@ pub async fn connect_mongodb() -> Result<String, String> {
 
 #[tauri::command]
 pub async fn list_databases() -> String {
-    let uri = "mongodb://localhost:27017";
+    dotenv().ok();
+    let uri = env::var("MONGODB_URI").unwrap_or_else(|_| "mongodb://localhost:27017".to_string());
     let client = Client::with_uri_str(uri).await.unwrap();
     let databases = client.list_databases().await.unwrap();
     format!("Databases: {:?}", databases)
